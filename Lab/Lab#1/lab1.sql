ffaf9c6a08828381d645296ec62653d0eed1a10d
ada-dmitry <ada.dmitry@gmail.com>
2023-09-27 11:27:46 +0300

    mod l1

--- Lab/Lab#1/lab1.sql
+++ Lab/Lab#1/lab1.sql
@@ -1,33 +1,32 @@
+/* Лабораторная работа #1 || Вариант 4 || Антипенко */
+
+
 CREATE table countries (
-  id numeric(3) PRIMARY KEY NOT NULL,
+    id numeric(3) PRIMARY KEY NOT NULL,
   
-  name_country varchar(64) NOT NULL
+    name_country varchar(64) NOT NULL
 );
 
 CREATE table schedules (
-  id integer PRIMARY KEY,
+    id integer PRIMARY KEY,
   
-  dt_dep timestamp without time zone NOT NULL,
-  dt_arr timestamp without time zone NOT NULL,
+    dt_dep timestamp without time zone NOT NULL,
+    dt_arr timestamp without time zone NOT NULL,
+    plс_of_dep varchar(2000) NOT NULL default 'Москва',
+    plc_of_arr varchar(2000) NOT NULL,
+    tpe varchar(9) NOT NULL 
+    CHECK(tpe = 'Скорый' OR tpe = 'Фирменный' OR tpe = 'Обычный'),
   
-  plс_of_dep varchar(2000) NOT NULL default 'Москва',
-  plc_of_arr varchar(2000) NOT NULL,
+    cnt_stops integer CHECK(cnt_stops > 0),
   
-  tpe varchar(9) NOT NULL
-  CHECK(tpe = 'Скорый' OR tpe = 'Фирменный' OR tpe = 'Обычный'),
-  
-  cnt_stops integer CHECK(cnt_stops > 0),
-  
-  id_oper numeric(3) references countries(id) not null
+    id_oper numeric(3) references countries(id) not null
 );
 
-INSERT into countries 
-  VALUES 
-  (321, 'Россия');
-  
-INSERT into schedules
-  VALUES(1, '1999-01-08 04:05:06',
-  '1999-01-08 04:05:06', default, 'СПб', 'Скорый', 40, 321);
+INSERT into countries VALUES 
+    (321, 'Россия');
+    
+INSERT into schedules VALUES
+    (1, '1999-01-08 04:05:06', '1999-01-08 04:05:06', default, 'СПб', 'Скорый', 40, 321);
 
 SELECT * FROM countries;
 SELECT * FROM schedules;
